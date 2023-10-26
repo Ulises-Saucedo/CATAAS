80d9378f019858e399a3baea02fcf1d6690f1dbe
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-10-26 19:34:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="h-[75px] bg-slate-700 text-white flex items-center justify-center">
+    <h1 class="text-4xl">CATAAS</h1>
+  </header>
+  <section class="p-4 grid grid-cols-5 gap-4">
+    <div v-for="element in data" class="inline-block pb-4 border-2">
+      <img :src="'https://cataas.com/cat/' + element._id" class="h-[200px] w-full">
+      <p v-for="e in element.tags" class="inline m-4">
+        {{ e }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data (){
+      return {
+        data: null
+      }
+    },
+    created(){
+      fetch('https://cataas.com/api/cats?tags=cute')
+        .then(response => response.json())
+        .then(dataJson => this.data = dataJson)
+    }
+  }
+</script>
